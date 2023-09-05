7205e6fcb814e83ce6af1bcf5649a7bef4758f3c
Carlos Marin <carlosmarin151288@gmail.com>
2023-09-05 15:47:14 -0300

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const GifExpertApp = () => {
+    return (
+        <>
+            {/* titulo */}
+            <h1>GifExpertApp</h1>
+            {/* input */}
+            {/* Listado de Gif */}
+                {/* Gif Item */}
+        </>
+    )
+}
