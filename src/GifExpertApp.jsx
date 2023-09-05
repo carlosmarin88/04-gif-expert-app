ace7ca22631d237c8b20f72e09b3ff7db661cf32
Carlos Marin <carlosmarin151288@gmail.com>
2023-09-05 17:55:56 -0300

    add list categories

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const GifExpertApp = () => {
+    
+    const [categories, setcategories] = useState([ 'One Punch', 'Dragon Ball']);
+
+    console.log(categories);
+
     return (
         <>
             {/* titulo */}
             <h1>GifExpertApp</h1>
             {/* input */}
             {/* Listado de Gif */}
+            <ol>
+                {categories.map( cat => <li key={cat}>{cat}</li>)}
+            </ol>
                 {/* Gif Item */}
         </>
     )
