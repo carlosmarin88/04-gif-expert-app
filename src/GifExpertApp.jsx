002661a8051fc3f007e9792634dda1f13d12e481
Carlos Marin <33185069+carlosmarin88@users.noreply.github.com>
2023-09-05 21:16:02 -0300

    add event onClick

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,7 +4,14 @@ export const GifExpertApp = () => {
     
     const [categories, setcategories] = useState([ 'One Punch', 'Dragon Ball']);
 
-    console.log(categories);
+    const onAddCategory = (event) => {
+        // Valorant
+        console.log('Valorant')
+        //setcategories([...categories, 'Valorant']);
+        setcategories(cat => [...cat, 'Valorant']); // insert al final
+        //setcategories(cat => ['Valorant', ...cat]); // insert al principio
+        
+    }
 
     return (
         <>
@@ -12,6 +19,7 @@ export const GifExpertApp = () => {
             <h1>GifExpertApp</h1>
             {/* input */}
             {/* Listado de Gif */}
+            <button onClick={onAddCategory}>Agregar</button>
             <ol>
                 {categories.map( cat => <li key={cat}>{cat}</li>)}
             </ol>
