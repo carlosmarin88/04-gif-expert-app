378d1f82610dfb796ecba4b0a4bfc7291f1f9df3
Carlos Marin <carlosmarin151288@gmail.com>
2023-09-07 18:02:38 -0300

    agregar servicio para obtener los gifs

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,14 +1,9 @@
+import { getGifs } from "../helpers/getGifs"
+
 export const GifGrid = ({ category }) => {
 
-
-    const getGifs = async () => {
-        const url = `https://api.giphy.com/v1/gifs/search?api_key=P9T459SXtZxy7nzWc1QBs7uVeElvExbl&q=${category}`;
-        const resp = await fetch(url);
-
-        console.log(resp);
-    }
+    getGifs(category)
     
-
     return (
         <>
             <h3>{category}</h3>
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,12 @@
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=P9T459SXtZxy7nzWc1QBs7uVeElvExbl&q=${category}&limit=20`;
+    const resp = await fetch(url);
+    const { data } = await resp.json(); 
+    const gifs = data.map(img=> ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }))
+    console.log('gifs',gifs);
+    return gifs;
+}
