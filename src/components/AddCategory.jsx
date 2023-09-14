58d12b636a6a99a5104f2476d3e1183650a5071d
Carlos Marin <carlosmarin151288@gmail.com>
2023-09-14 15:17:13 -0300

    Agregar test al componente addCategory

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,8 +1,9 @@
 // para crear un componente shortcut rafc
 
 import { useState } from "react"
+import { PropTypes } from "prop-types"
 
-export const AddCategory = ({onNewCategory}) => { //onAddonCategory
+export const AddCategory = ({ onNewCategory }) => { //onAddonCategory
 
     const [inputValue, setInputValue] = useState('');
 
@@ -16,7 +17,7 @@ export const AddCategory = ({onNewCategory}) => { //onAddonCategory
     const onSubmit = (event) => {
         event.preventDefault();
 
-        if(inputValue.trim().length <= 1) return;
+        if (inputValue.trim().length <= 1) return;
         //console.log(event);
         //console.log(inputValue);
         //console.log(onAddCategory);
@@ -26,8 +27,8 @@ export const AddCategory = ({onNewCategory}) => { //onAddonCategory
     }
 
     return (
-        <form onSubmit={onSubmit}>
-            <input 
+        <form onSubmit={onSubmit} aria-label="form">
+            <input
                 type="text"
                 placeholder="Buscar Gifs"
                 value={inputValue}
@@ -36,4 +37,8 @@ export const AddCategory = ({onNewCategory}) => { //onAddonCategory
         </form>
 
     )
+}
+
+AddCategory.propTypes = {
+    onNewCategory: PropTypes.func.isRequired
 }
---
+++ tests/components/AddCategory.test.jsx
@@ -0,0 +1,53 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import { AddCategory } from "../../src/components"
+
+describe('Pruebas en <AddCategory/>', () => {
+
+    test('Debe de cambiar el valor de la caja de texto', () => {
+        render(<AddCategory onNewCategory={() => { }} />);
+        const input = screen.getByRole('textbox');
+
+        fireEvent.input(input, { target: {value: 'Saitama'}});
+        //screen.debug();
+        expect(input.value).toBe('Saitama');
+
+    });
+
+    test('Debe de llamar onNewCategory si el input tiene un valor', () => { 
+        const inputValue = 'Saitama';
+        const onNewCategory = jest.fn(); // para usar mocks
+
+        render(<AddCategory onNewCategory={onNewCategory} />);
+        
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+
+        fireEvent.input(input, { target: {value: inputValue}});
+        //screen.debug();
+        fireEvent.submit(form);
+
+        expect(input.value).toBe('');
+        expect(onNewCategory).toHaveBeenCalled(); // expect(onNewCategory).toHaveBeenCalledTimes(1);
+        expect(onNewCategory).toHaveBeenCalledWith(inputValue);
+        
+
+     })
+
+     test('No debe de llamar el onNewCategory si el input esta vacio', () => { 
+
+        const inputValue = 'S';
+        const onNewCategory = jest.fn(); // para usar mocks
+
+        render(<AddCategory onNewCategory={onNewCategory} />);
+
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+
+
+        fireEvent.input(input, { target: {value: inputValue}});
+        fireEvent.submit(form);
+
+        expect(onNewCategory).not.toHaveBeenCalled(); // expect(onNewCategory).toHaveBeenCalledTimes(0);
+    
+      })
+});
