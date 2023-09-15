b2a22f86d6fcd7375744f60f7edba131c27bf59e
Carlos Marin <carlosmarin151288@gmail.com>
2023-09-15 10:36:50 -0300

    Falta terminar las pruebas en GifExpertApp

---
+++ tests/GifExpertApp.test.jsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react"
+import { GifExpertApp } from "../src/GifExpertApp"
+
+describe('Pruebas en el componente <GifExpertApp/>', () => { 
+
+    test('Debe de hacer match con el snapshot', () => { 
+
+        const {container} = render(<GifExpertApp/>);
+        expect(container).toMatchSnapshot();
+     })
+
+
+     test('Debe de llamar onNewCategory si el input tiene un valor', async() => { 
+
+        render(<GifExpertApp/>);
+        //const addCategory = await screen.findByTestId('add-category');
+        //console.log(addCategory);
+     })
+ })
